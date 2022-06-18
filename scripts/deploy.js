14ef8b8581228b5bb7a971ea2a922c1a62d0fa6a
Boban Milošević <milosevic.boban@gmail.com>
2022-06-18 16:08:36 +0200

    Added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const { CONTRACT_NAME, VRF_SUBSCRIPTION_ID } = require('../app.config')
+
+async function main () {
+
+    const contractFactory = await ethers.getContractFactory(CONTRACT_NAME)
+    const contract = await contractFactory.deploy(VRF_SUBSCRIPTION_ID) // pass contract constructor arguments to deploy() method
+
+    console.log(`Transaction ${contract.deployTransaction.hash} is deploying contract ${contract.address}`)
+    await contract.deployed()
+    console.log('Contract successfully deployed')
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
