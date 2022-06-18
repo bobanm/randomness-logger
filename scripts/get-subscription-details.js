d2fa4e87b2dfb597eaec22924f3297e313629e20
Boban Milošević <milosevic.boban@gmail.com>
2022-06-18 16:08:15 +0200

    Moved config params to app.config.js

--- scripts/get-subscription-details.js
+++ scripts/get-subscription-details.js
@@ -1,16 +1,11 @@
 const { ethers } = require('hardhat')
 
-const CONTRACT_ADDRESS = '0x6168499c0cFfCaCD319c818142124B7A15E857ab'
-const CONTRACT_ABI = [
-    'function getSubscription(uint64 subId) external view returns (uint96 balance, uint64 reqCount, address owner, address[] memory consumers)',
-]
-const SUBSCRIPTION_ID = 5853
+const { VRF_CONTRACT_ADDRESS, VRF_CONTRACT_ABI, VRF_SUBSCRIPTION_ID } = require('../app.config')
 
 async function main () {
 
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, ethers.provider)
-
-    const subscription = await contract.getSubscription(SUBSCRIPTION_ID)
+    const Vrf = new ethers.Contract(VRF_CONTRACT_ADDRESS, VRF_CONTRACT_ABI, ethers.provider)
+    const subscription = await Vrf.getSubscription(VRF_SUBSCRIPTION_ID)
     console.log(subscription)
 }
 
--- scripts/request-random-number.js
+++ scripts/request-random-number.js
@@ -1,16 +1,13 @@
 const { ethers } = require('hardhat')
 
-const CONTRACT_ADDRESS = '0x03c9c44378CE78e7D30ae90645024d0212e269B5' // rinkeby
-const CONTRACT_NAME = 'RandomnessLogger'
-
-const CALLBACK_GAS_LIMIT = 100000
+const { CONTRACT_NAME, CONTRACT_ADDRESS, CALLBACK_GAS_LIMIT } = require('../app.config')
 
 async function main () {
 
     const signer = ethers.provider.getSigner()
-    const contract = await ethers.getContractAt(CONTRACT_NAME, CONTRACT_ADDRESS, signer)
+    const RandomnessLogger = await ethers.getContractAt(CONTRACT_NAME, CONTRACT_ADDRESS, signer)
 
-    const transaction = await contract.requestRandomNumbers(CALLBACK_GAS_LIMIT)
+    const transaction = await RandomnessLogger.requestRandomNumbers(CALLBACK_GAS_LIMIT)
     const receipt = await transaction.wait()
     console.log(`Request ID = ${receipt.events[1].args.requestId}`)
 }
