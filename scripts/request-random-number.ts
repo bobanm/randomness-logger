14aa8817dffc81bebf19fd24745412c492586137
Boban Milošević <milosevic.boban@gmail.com>
2022-06-22 11:24:14 +0200

    Added explicit type for response of requestRandomNumbers

--- scripts/request-random-number.ts
+++ scripts/request-random-number.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'hardhat'
+import { ContractTransaction } from 'ethers'
 
 import { CONTRACT_NAME, CONTRACT_ADDRESS, CALLBACK_GAS_LIMIT } from '../app.config'
 
@@ -7,10 +8,10 @@ async function main () {
     const signer = ethers.provider.getSigner()
     const RandomnessLogger = await ethers.getContractAt(CONTRACT_NAME, CONTRACT_ADDRESS, signer)
 
-    const transaction = await RandomnessLogger.requestRandomNumbers(CALLBACK_GAS_LIMIT)
-    console.log('Requested random number. Waiting for the transaction to be mined...')
+    const transaction: ContractTransaction = await RandomnessLogger.requestRandomNumbers(CALLBACK_GAS_LIMIT)
+    console.log('Requested a random number. Waiting for the transaction to be mined...')
     const receipt = await transaction.wait()
-    console.log(`Request ID = ${receipt.events[1].args.requestId}`)
+    console.log(`Request ID = ${receipt.events![1].args!.requestId}`)
 }
 
 main()
