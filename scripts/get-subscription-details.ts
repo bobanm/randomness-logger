45a7186ffcdbaf2f326c493574a480b2426d5ed7
Boban Milošević <milosevic.boban@gmail.com>
2022-06-22 19:27:30 +0200

    Nicer output formatting in `get-subscription-details`

--- scripts/get-subscription-details.ts
+++ scripts/get-subscription-details.ts
@@ -1,12 +1,30 @@
 import { ethers } from 'hardhat'
+import { BigNumber } from 'ethers'
 
 import { VRF_CONTRACT_ADDRESS, VRF_CONTRACT_ABI, VRF_SUBSCRIPTION_ID } from '../app.config'
 
 async function main () {
 
+    type SubscriptionResponse = {
+        owner: string,
+        balance: BigNumber,
+        reqCount: BigNumber,
+        consumers: string[],
+    }
+
     const Vrf = new ethers.Contract(VRF_CONTRACT_ADDRESS, VRF_CONTRACT_ABI, ethers.provider)
-    const subscription = await Vrf.getSubscription(VRF_SUBSCRIPTION_ID)
-    console.log(subscription)
+    const subscription: SubscriptionResponse = await Vrf.getSubscription(VRF_SUBSCRIPTION_ID)
+
+    console.log(`owner      ${subscription.owner}`)
+    console.log(`balance    ${ethers.utils.formatUnits(subscription.balance)} LINK`)
+    console.log(`fulfilled  ${subscription.reqCount.toNumber()} requests`)
+    
+    if (subscription.consumers) {
+        console.log('\nconsumers')
+        for (const consumer of subscription.consumers) {
+            console.log(`  ${consumer}`)
+        }
+    }
 }
 
 main()
