10266854ebb272fddc49a17b27ffede52863e0f5
Boban Milošević <milosevic.boban@gmail.com>
2026-06-09 18:53:35 +0200

    🪟 get-numbers-history script now prints the events table using TablePrinter

--- packages/backend/scripts/get-numbers-history.ts
+++ packages/backend/scripts/get-numbers-history.ts
@@ -1,25 +1,22 @@
 import hre from 'hardhat'
-
+import { TablePrinter } from '@bobanm/table-printer'
 import '@randomness-logger/shared'
 import { CONTRACT_ADDRESS, CONTRACT_NAME, CONTRACT_BLOCK_DEPLOYED } from '../../../config/app.config'
-
-type HistoryEntry = {
-    requestBlockNumber: bigint,
-    requestTimestamp: bigint,
-    requestId: bigint,
-    requestorAddress: string,
-    responseBlockNumber?: bigint,
-    responseTimestamp?: bigint,
-    randomNumber?: bigint,
-}
 
 const { ethers } = await hre.network.create()
 
 const RandomnessLogger = await ethers.getContractAt(CONTRACT_NAME, CONTRACT_ADDRESS)
 
-const history: HistoryEntry[] = []
 const filterRequests = RandomnessLogger.filters.NumberRequested()
 const filterResponses = RandomnessLogger.filters.NumberReceived()
+
+const historyTable = new TablePrinter(
+    [['REQUEST ID', 'REQUEST DATE', 'RESPONSE DATE', 'REQ BLK', 'RESP BLK', 'REQUESTOR ADDR', 'RANDOM NUMBER']],
+    {
+        rightAlignedColumns: [3, 4],
+        repeatHeaderAtBottom: true,
+    }
+)
 
 console.log('\nQuerying the blockchain, please wait...\n')
 
@@ -28,44 +25,24 @@ const [requestsLog, responsesLog] = await Promise.all([
     RandomnessLogger.queryFilter(filterResponses, CONTRACT_BLOCK_DEPLOYED),
 ])
 
-for (const requestsLogEntry of requestsLog) {
-    if (!('args' in requestsLogEntry)) continue
-    const { args: reqArgs } = requestsLogEntry
+// search through responses log array, match with requests array on requestId
+for (const responsesLogEntry of responsesLog) {
+    for (const requestsLogEntry of requestsLog) {
+        if (responsesLogEntry.args.requestId === requestsLogEntry.args.requestId) {
 
-    const historyEntry: HistoryEntry = {
-        requestBlockNumber: reqArgs.blockNumber,
-        requestTimestamp: reqArgs.timestamp,
-        requestId: reqArgs.requestId,
-        requestorAddress: reqArgs.requestorAddress,
-    }
-
-    for (const responsesLogEntry of responsesLog) {
-        if (!('args' in responsesLogEntry)) continue
-        const { args: resArgs } = responsesLogEntry
-
-        if (resArgs.requestId.toString() === reqArgs.requestId.toString()) {
-            historyEntry.responseBlockNumber = resArgs.blockNumber
-            historyEntry.responseTimestamp = resArgs.timestamp
-            historyEntry.randomNumber = resArgs.randomNumber
+            historyTable.addRow([
+                requestsLogEntry.args.requestId.toString().ellipsify(7, 3),
+                new Date(Number(requestsLogEntry.args.timestamp) * 1000).toNiceString(),
+                new Date(Number(responsesLogEntry.args.timestamp) * 1000).toNiceString(),
+                requestsLogEntry.args.blockNumber,
+                responsesLogEntry.args.blockNumber,
+                requestsLogEntry.args.requestorAddress.ellipsify(8, 3),
+                responsesLogEntry.args.randomNumber.toString().ellipsify(9, 3),
+            ])
 
             break
         }
     }
-
-    history.push(historyEntry)
 }
 
-console.log('REQUEST ID      REQUEST DATE          RESPONSE DATE         REQ BLK    RESP BLK   REQUESTOR ADDR   RANDOM NUMBER')
-console.log('-'.repeat(112))
-
-for (const historyEntry of history) {
-    const requestId = historyEntry.requestId.toString().ellipsify(7, 3)
-    const requestDate = new Date(Number(historyEntry.requestTimestamp) * 1000).toNiceString()
-    const responseDate = historyEntry.responseTimestamp ? new Date(Number(historyEntry.responseTimestamp) * 1000).toNiceString() : ''.padEnd(19)
-    const requestBlockNumber = historyEntry.requestBlockNumber.toString().padEnd(8)
-    const responseBlockNumber = historyEntry.responseBlockNumber?.toString().padEnd(8) ?? ''.padEnd(8)
-    const requestorAddress = historyEntry.requestorAddress.ellipsify(8, 3)
-    const randomNumber = historyEntry.randomNumber?.toString().ellipsify(7, 3) ?? ''
-
-    console.log(`${requestId}   ${requestDate}   ${responseDate}   ${requestBlockNumber}   ${responseBlockNumber}   ${requestorAddress}   ${randomNumber}`)
-}
+console.log(historyTable.toString())
